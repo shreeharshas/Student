254a2e642dc42da86f92756dbee890cf2716e5ff
Shree Harsha Sridharamurthy <sridhash@iu.edu>
2017-02-26 23:55:42 -0500

    Add project files.

---
+++ DashBoardMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DashBoardMVC.Startup))]
+namespace DashBoardMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
